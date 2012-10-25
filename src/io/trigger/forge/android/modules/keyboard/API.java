557684911878db1ea2866f4ad6fea61a01f6dde8
Michael Marsh <michael@michael-Aspire-5755.(none)>
2012-10-24 21:11:12 -0400

    attempting a fix of ui thread exception

--- src/io/trigger/forge/android/modules/keyboard/API.java
+++ src/io/trigger/forge/android/modules/keyboard/API.java
@@ -11,10 +11,18 @@ public class API {
 	public static KeyCharacterMap map = KeyCharacterMap.load(KeyCharacterMap.VIRTUAL_KEYBOARD);
 	
 	public static void typestring(final ForgeTask task, @ForgeParam("input") final String input){
-		Activity activity = ForgeApp.getActivity();
-		KeyEvent[] events = map.getEvents(input.toCharArray());
-		for(KeyEvent e : events){
-			activity.dispatchKeyEvent(e);
-		}
+		final Activity activity = ForgeApp.getActivity();
+		final KeyEvent[] events = map.getEvents(input.toCharArray());
+		activity.runOnUiThread(new Runnable(){
+
+			@Override
+			public void run() {
+				for(KeyEvent e : events)
+					activity.dispatchKeyEvent(e);
+				
+			}
+			
+		});
+		
 	}
 }
