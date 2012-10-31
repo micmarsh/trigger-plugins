6b53ec28cbecd73c369eebcc7e7b7fc3706844c7
Michael Marsh <michael@michael-Aspire-5755.(none)>
2012-10-31 13:02:46 -0400

    Added "event" module, currently dispatches native touchevents by position

---
+++ src/io/trigger/forge/android/modules/event/API.java
@@ -0,0 +1,41 @@
+package io.trigger.forge.android.modules.event;
+
+
+import android.os.SystemClock;
+import android.view.MotionEvent;
+import android.webkit.WebView;
+import io.trigger.forge.android.core.ForgeActivity;
+import io.trigger.forge.android.core.ForgeApp;
+import io.trigger.forge.android.core.ForgeParam;
+import io.trigger.forge.android.core.ForgeTask;
+
+public class API {
+	
+	public static void touch(final ForgeTask task,
+			@ForgeParam("x") final int x, @ForgeParam("y") final int y){
+		ForgeActivity activity = ForgeApp.getActivity();
+		final WebView webView = activity.webView;
+		activity.runOnUiThread(new Runnable() {
+        	public void run() {
+        			long uptime = SystemClock.uptimeMillis();
+	        		webView.dispatchTouchEvent(MotionEvent.obtain(
+	        				uptime,
+	        				uptime, 
+	        				MotionEvent.ACTION_DOWN, 
+	        				x,
+	        				y,
+	        				0)
+	        		);
+	        		webView.dispatchTouchEvent(MotionEvent.obtain(
+	        				uptime, 
+	        				uptime, 
+	        				MotionEvent.ACTION_UP, 
+	        				x,
+	        				y,
+	        				0)
+	        		);
+        	}
+	
+		});
+	}
+}
